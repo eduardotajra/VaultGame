0d9e819526687fcdbec8e1925f180914367c5887
FcoArthur <arthurnunesmontenegro@gmail.com>
2024-09-30 12:38:54 -0300

    alteração na navbar
    • hover no icone de pesquisa
    • icone de carrinho(sem contador para itens)
    • icone de login com hover
    
    Co-Authored-By: Eduardo de Morais Tajra <103423149+eduardotajra@users.noreply.github.com>
    Co-Authored-By: Natan Pucci <natan.puccib@outlook.com>
    Co-Authored-By: Lucas <104266998+lucas-twygz@users.noreply.github.com>

--- vault-game-front/src/componentes/Navbar.js
+++ vault-game-front/src/componentes/Navbar.js
@@ -1,66 +1,97 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ScrollReveal from "scrollreveal";
 import styles from "./Navbar.module.css";
 import logo from "./img/logo.png";
 import lupa from "./img/lupa.png";
+import carrinho from "./img/carrinho.png";
+import login from "./img/login.png";
 
-function Navbar(){
-    useEffect(() => {
-        ScrollReveal().reveal(".page-header", {
-          origin: "top",
-          distance: "100px",
-          duration: 1000,
-          reset: false,
-        });
-      }, []);
-        
+function Navbar() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    return(
-        <>
-        <header id="page-header">
-            <div className={styles.interface}>
+  useEffect(() => {
+    ScrollReveal().reveal(".page-header", {
+      origin: "top",
+      distance: "100px",
+      duration: 1000,
+      reset: false,
+    });
+  }, []);
 
-                <div className={styles.logo}>
-                    <a className="logo" href="index.html">
-                        <img src={logo} alt="Logo"/>
-                    </a>
-                </div>
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
-                <nav className={styles.menu.desktop}>
+  return (
+    <>
+      <header id="page-header">
+        <div className={styles.interface}>
+          <div className={styles.logo}>
+            <a className="logo" href="index.html">
+              <img src={logo} alt="Logo" />
+            </a>
+          </div>
 
-                    <ul className={styles.menu}>
-                        <li>
-                            <a href="#Ofertas">Ofertas</a>
-                        </li>
-                        <li>
-                            <a href="#PC">PC</a>
-                        </li>
-                        <li>
-                            <a href="#Xbox">Xbox</a>
-                        </li>
-                        <li>
-                            <a href="#PlayStation">PlayStation</a>
-                        </li>
-                        <li>
-                            <a href="#Nintendo">Nintendo</a>
-                        </li>
-                    </ul>
+          <nav className={styles.menu.desktop}>
+            <ul className={styles.menu}>
+              <li>
+                <a href="#Ofertas">Ofertas</a>
+              </li>
+              <li>
+                <a href="#PC">PC</a>
+              </li>
+              <li>
+                <a href="#Xbox">Xbox</a>
+              </li>
+              <li>
+                <a href="#PlayStation">PlayStation</a>
+              </li>
+              <li>
+                <a href="#Nintendo">Nintendo</a>
+              </li>
+            </ul>
+          </nav>
 
-                </nav>
+          <div className={styles.search}>
+            <input type="text" id="busca" placeholder="Buscar" required />
 
-                <div className={styles.search}>
-                    <input type="text" id="busca" placeholder="Buscar" required/>
+            <a href="">
+              <img id="lupa" src={lupa} alt="Mostrar/Esconder" />
+            </a>
+          </div>
 
-                    <a href="">
-                        <img id="lupa" src={lupa} alt="Mostrar/Esconder"/>
-                    </a>
-                </div>
+          <div className={styles.compras}>
+            <a href="#CarrinhoDeCompras">
+              <img id="carrinho" src={carrinho} alt="Mostrar/Esconder" />
+            </a>
+          </div>
 
+          <div className={styles.login}>
+            <div className={styles.loginIcone}>
+              <button className={styles.loginbotao}>
+                <img src={login} alt="Logo" className="navbar-logo" />
+              </button>
+              {/* Mover o dropdown aqui para que seja um irmão do botão */}
+              <div
+                className={`${styles.dropdown} ${
+                  isDropdownOpen ? styles.show : ""
+                }`}
+              >
+                <ul>
+                  <li>
+                    <a href="/signup">Criar Conta</a>
+                  </li>
+                  <li>
+                    <a href="/login">Fazer Login</a>
+                  </li>
+                </ul>
+              </div>
             </div>
-        </header>
-        </>
-    );
-
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Navbar;
