0fa31dccd3a28b5dc710c54d0d54cf56218b9f72
Natan Pucci <natan.puccib@outlook.com>
2024-10-01 11:15:13 -0300

    Alterações e adições das imagens na navBar
    Adicionadas as imagens na navBar e alteração da cor do carrinho
    
    Co-Authored-By: FcoArthur <183325787+FcoArthur@users.noreply.github.com>
    Co-Authored-By: Eduardo de Morais Tajra <103423149+eduardotajra@users.noreply.github.com>
    Co-Authored-By: Lucas <104266998+lucas-twygz@users.noreply.github.com>

--- vault-game-front/src/componentes/Navbar.js
+++ vault-game-front/src/componentes/Navbar.js
@@ -6,6 +6,11 @@ import lupa from "./img/lupa.png";
 import carrinho from "./img/carrinho.png";
 import login from "./img/login.png";
 import Cadastro from "./Cadastro.js"
+import nintendo from "./img/navBarImg/nintendo.png"
+import pc from "./img/navBarImg/pc.png"
+import ofertas from "./img/navBarImg/ofertas.png"
+import playstation from "./img/navBarImg/playstation.png"
+import xbox from "./img/navBarImg/xbox.png"
 
 function Navbar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -37,19 +42,42 @@ function Navbar() {
             <nav className={styles.menu.desktop}>
               <ul className={styles.menu}>
                 <li>
-                  <a href="#Ofertas">Ofertas</a>
+
+                      <a href="#Ofertas" className={styles.icons}>
+                        <img src={ofertas}></img>
+                        Ofertas
+                      </a>
+
                 </li>
                 <li>
-                  <a href="#PC">PC</a>
+
+                    <a href="#PC" className={styles.icons}>
+                      <img src={pc}></img>
+                      PC
+                    </a>
+
                 </li>
                 <li>
-                  <a href="#Xbox">Xbox</a>
+
+                    <a href="#Xbox" className={styles.icons}>
+                      <img src={xbox}></img>
+                      Xbox
+                    </a>
+
                 </li>
                 <li>
-                  <a href="#PlayStation">PlayStation</a>
+
+                    <a href="#Playstation" className={styles.icons}>
+                      <img src={playstation}></img>
+                      Playstation
+                    </a>
+
                 </li>
                 <li>
-                  <a href="#Nintendo">Nintendo</a>
+                    <a href="#Nintendo" className={styles.icons}>
+                      <img src={nintendo}></img>
+                      Nintendo
+                    </a>
                 </li>
               </ul>
             </nav>
