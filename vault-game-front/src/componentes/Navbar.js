651d21ce2a2fe5837761871f22dbdc9729b6208e
FcoArthur <arthurnunesmontenegro@gmail.com>
2024-10-01 10:15:37 -0300

    icones na navbar
    • adicionados icones na navbar
    
    Co-Authored-By: Eduardo de Morais Tajra <103423149+eduardotajra@users.noreply.github.com>
    Co-Authored-By: Natan Pucci <natan.puccib@outlook.com>
    Co-Authored-By: Lucas <104266998+lucas-twygz@users.noreply.github.com>

--- vault-game-front/src/componentes/Navbar.js
+++ vault-game-front/src/componentes/Navbar.js
@@ -5,7 +5,11 @@ import logo from "./img/logo.png";
 import lupa from "./img/lupa.png";
 import carrinho from "./img/carrinho.png";
 import login from "./img/login.png";
-import Cadastro from "./Cadastro.js"
+import Cadastro from "./Cadastro.js";
+import pc from "./img/pc.png";
+import xbox from "./img/xbox.png";
+import ps from "./img/ps.png";
+import nitendo from "./img/nintendo.png";
 
 function Navbar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -37,19 +41,31 @@ function Navbar() {
             <nav className={styles.menu.desktop}>
               <ul className={styles.menu}>
                 <li>
+                  {/* um icone aqui ?*/}
                   <a href="#Ofertas">Ofertas</a>
                 </li>
                 <li>
-                  <a href="#PC">PC</a>
+                  <a href="#PC">
+                    <img src={pc} />
+                    PC
+                  </a>
                 </li>
                 <li>
-                  <a href="#Xbox">Xbox</a>
+                  <a href="#Xbox">
+                    <img src={xbox} />
+                    Xbox
+                  </a>
                 </li>
                 <li>
-                  <a href="#PlayStation">PlayStation</a>
+                  <a href="#PlayStation">
+                    <img src={ps} /> PlayStation
+                  </a>
                 </li>
                 <li>
-                  <a href="#Nintendo">Nintendo</a>
+                  <a href="#Nintendo">
+                    <img src={nitendo} />
+                    Nintendo
+                  </a>
                 </li>
               </ul>
             </nav>
@@ -57,9 +73,13 @@ function Navbar() {
               <input type="text" id="busca" placeholder="Buscar" required />
 
               <a className={styles.linko} href="">
-                <img className={styles.lupa} id="lupa" src={lupa} alt="Mostrar/Esconder" />
+                <img
+                  className={styles.lupa}
+                  id="lupa"
+                  src={lupa}
+                  alt="Mostrar/Esconder"
+                />
               </a>
-
             </div>
           </div>
 
@@ -71,7 +91,6 @@ function Navbar() {
             </div>
 
             <div className={styles.loginIcone}>
-
               <button className={styles.loginbotao}>
                 <img src={login} alt="Logo" className="navbar-logo" />{" "}
                 <p> Entrar </p>
@@ -88,11 +107,8 @@ function Navbar() {
                 </ul>
               </div>
             </div>
-
           </div>
-
         </div>
-
       </header>
     </>
   );
