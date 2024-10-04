9926c1f8a6e5045672397a2cb1e00a587cb49482
Lucas <104266998+lucas-twygz@users.noreply.github.com>
2024-10-04 16:17:21 -0300

    correcoes
    redirecionamento na barra de fazer login, correcoes nos estilos dos botoes.
    
    Co-Authored-By: FcoArthur <183325787+FcoArthur@users.noreply.github.com>
    Co-Authored-By: Natan Pucci <natan.puccib@outlook.com>
    Co-Authored-By: Eduardo de Morais Tajra <103423149+eduardotajra@users.noreply.github.com>

--- vault-game-front/src/componentes/Navbar.js
+++ vault-game-front/src/componentes/Navbar.js
@@ -1,19 +1,19 @@
 import React, { useEffect } from "react";
-import { Link, Outlet, useNavigate } from "react-router-dom"
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import ScrollReveal from "scrollreveal";
 import styles from "./Navbar.module.css";
 import logo from "./img/logo.png";
 import lupa from "./img/lupa.png";
 import carrinho from "./img/carrinho.png";
 import sigIn from "./img/login.png";
-import nintendo from "./img/navBarImg/nintendo.png"
-import pc from "./img/navBarImg/pc.png"
-import ofertas from "./img/navBarImg/ofertas.png"
-import playstation from "./img/navBarImg/playstation.png"
-import xbox from "./img/navBarImg/xbox.png"
+import nintendo from "./img/navBarImg/nintendo.png";
+import pc from "./img/navBarImg/pc.png";
+import ofertas from "./img/navBarImg/ofertas.png";
+import playstation from "./img/navBarImg/playstation.png";
+import xbox from "./img/navBarImg/xbox.png";
 
 function Navbar() {
-
+  // Efeito de ScrollReveal
   useEffect(() => {
     ScrollReveal().reveal(".page-header", {
       origin: "top",
@@ -23,77 +23,69 @@ function Navbar() {
     });
   }, []);
 
+
   const navigate = useNavigate();
 
-  function login(estado){
+  // função pra mandar o estado para o troca.js
+  function login(estado) {
     navigate("/cadastro", {
-      state: estado,
+      state: { ativo: estado },
     });
-}
+  }
 
   return (
     <>
       <header id="page-header">
         <div className={styles.interface}>
-
           <div className={styles.logo}>
             <Link to="/">
-            <a className="logo" href="index.html">
-              <img src={logo} alt="Logo" />
-            </a>
+              <a className="logo" href="index.html">
+                <img src={logo} alt="Logo" />
+              </a>
             </Link>
           </div>
 
           <div className={styles.central}>
-              <ul className={styles.menu}>
-                <li>
-
-                      <a href="#Ofertas" className={styles.icons}>
-                        <img src={ofertas} alt="Ofertas"></img>
-                        Ofertas
-                      </a>
-
-                </li>
-                <li>
-
-                    <a href="#PC" className={styles.icons}>
-                      <img src={pc} alt="Pc"></img>
-                      PC
-                    </a>
-
-                </li>
-                <li>
-
-                    <a href="#Xbox" className={styles.icons}>
-                      <img src={xbox} alt="Xbox"></img>
-                      Xbox
-                    </a>
-
-                </li>
-                <li>
-
-                    <a href="#Playstation" className={styles.icons}>
-                      <img src={playstation} alt="Playstation"></img>
-                      Playstation
-                    </a>
-
-                </li>
-                <li>
-                    <a href="#Nintendo" className={styles.icons}>
-                      <img src={nintendo} alt="Nintendo"></img>
-                      Nintendo
-                    </a>
-                </li>
-              </ul>
+            <ul className={styles.menu}>
+              <li>
+                <a href="#Ofertas" className={styles.icons}>
+                  <img src={ofertas} alt="Ofertas" />
+                  Ofertas
+                </a>
+              </li>
+              <li>
+                <a href="#PC" className={styles.icons}>
+                  <img src={pc} alt="Pc" />
+                  PC
+                </a>
+              </li>
+              <li>
+                <a href="#Xbox" className={styles.icons}>
+                  <img src={xbox} alt="Xbox" />
+                  Xbox
+                </a>
+              </li>
+              <li>
+                <a href="#Playstation" className={styles.icons}>
+                  <img src={playstation} alt="Playstation" />
+                  Playstation
+                </a>
+              </li>
+              <li>
+                <a href="#Nintendo" className={styles.icons}>
+                  <img src={nintendo} alt="Nintendo" />
+                  Nintendo
+                </a>
+              </li>
+            </ul>
           </div>
 
           <div className={styles.search}>
             <input type="text" id="busca" placeholder="Buscar" required />
 
             <a className={styles.linko} href="/">
-              <img className={styles.lupa} id="lupa" src={lupa} alt="Mostrar/Esconder"/>
+              <img className={styles.lupa} id="lupa" src={lupa} alt="Mostrar/Esconder" />
             </a>
-
           </div>
 
           <div className={styles.rightNav}>
@@ -104,7 +96,6 @@ function Navbar() {
             </div>
 
             <div className={styles.loginIcone}>
-
               <button className={styles.loginbotao}>
                 <img src={sigIn} alt="Logo" className="navbar-logo" />
                 <p> Entrar </p>
@@ -113,19 +104,18 @@ function Navbar() {
               <div className={styles.dropdown}>
                 <ul className={styles.ulDropdown}>
                   <li>
-                    <button onClick={() => login("login")}>Fazer Login</button>
+                    {/* vai mandar login como "login" */}
+                    <button onClick={() => login("login")}>Fazer Login</button> 
                   </li>
                   <li>
-                      <button onClick={() => login("cadastro")}>Criar Conta</button>
+                    {/* vai mandar login como "cadastro" */}
+                    <button onClick={() => login("cadastro")}>Criar Conta</button>
                   </li>
                 </ul>
               </div>
             </div>
-
           </div>
-
         </div>
-
       </header>
       <Outlet />
     </>
--- vault-game-front/src/componentes/Troca.js
+++ vault-game-front/src/componentes/Troca.js
@@ -1,32 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom"; // Importando useLocation
 import Login from "./Login";
 import Cadastro from "./Cadastro";
 import styles from "./Troca.module.css";
 
 function Troca() {
-  const [ativo, setAtivo] = useState("login");
+
+  const location = useLocation(); // hook de navegacao
+  const [ativo, setAtivo] = useState("login"); // criando variaveis e setando valor padrao pra login
+
+  useEffect(() => {
+    if (location.state && location.state.ativo) {
+      setAtivo(location.state.ativo); // Atualizando o estado com base na navegação
+    }
+  }, [location]);
+
   return (
     <div className={styles.container}>
       <div className={styles.botaoContainer}>
-
         <button
-          className={`${styles.botao} ${ativo === "login" ? styles.ativo : ""}`}
-          onClick={() => setAtivo("login")}
+          className={`${styles.botaoDiv} ${ativo === "login" ? styles.botaoDivOn : ""}`} // troca o valor de ativo para "login"
+          onClick={() => setAtivo("login")} // quando clicar no botao login ele passa o valor de "ativo" pra funcao setAtivo
         >
           Login
         </button>
         <button
-          className={`${styles.botao} ${ativo === "cadastro" ? styles.ativo : ""}`}
-          onClick={() => setAtivo("cadastro")}
+          className={`${styles.botaoDiv} ${ativo === "cadastro" ? styles.botaoDivOn : ""}`} // mesmo coisa la de cima
+          onClick={() => setAtivo("cadastro")} // msm coisa aqui, so que para cadastro
         >
           Cadastro
         </button>
       </div>
 
       <div className={styles.componenteContainer}>
-
+        {/* ativo eh login? entao vai receber voltar a pagina login, caso contrario recebe cadastro :) */}
         {ativo === "login" ? <Login /> : <Cadastro />}
       </div>
+
     </div>
   );
 }
