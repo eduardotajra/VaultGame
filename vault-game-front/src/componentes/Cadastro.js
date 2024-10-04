ac06d9f3eb024d98d0168bd7472ddf2380b6010c
FcoArthur <arthurnunesmontenegro@gmail.com>
2024-10-04 01:39:54 -0300

    tratamento de erro no cadastro
    • deixei comentado duvidas e algumas coisas que eu fiz
    • ainda não terminado
    
    Co-Authored-By: Natan Pucci <natan.puccib@outlook.com>
    Co-Authored-By: Lucas <104266998+lucas-twygz@users.noreply.github.com>
    Co-Authored-By: Eduardo de Morais Tajra <103423149+eduardotajra@users.noreply.github.com>

--- vault-game-front/src/componentes/Cadastro.js
+++ vault-game-front/src/componentes/Cadastro.js
@@ -15,11 +15,18 @@ function Cadastro() {
       reset: false,
     });
   }, []);
-
+  // declarações -- tutu
   const location = useLocation();
   const [pagina, setPagina] = useState(location.state);
   const [mostrarSenha, setMostrarSenha] = useState(false);
-
+  // ver se está vazio e erro - tutu
+  const [formDado, setFormDado] = useState({
+    nome: "",
+    email: " ",
+    senha: "",
+  });
+  const [erro, setErro] = useState({});
+  // acho que da para aproveitar essa parte que o duardo fez para reduzir o codigo- tutu
   const [nome, setNome] = useState(""); // cria a variavel de nome vazia que dps é setada pelo setNome no <input> de nome
   const [email, setEmail] = useState(""); // cria a variavel email vazia que dps é setada pelo setEmail no <input> de email
   const [senha, setSenha] = useState(""); // cria a variavel senha vazia que dps é setada pelo setSenha no <input> de senha
@@ -28,7 +35,7 @@ function Cadastro() {
   useEffect(() => {
     setPagina(location.state);
   }, [location.state]);
-
+  //  nao entendi esse alterarPagina :c e minha defesa era quase 1h da manhã e eu tinha que acordar 5h -tutu
   const alterarPagina = (novaPagina) => {
     setPagina(novaPagina);
   };
@@ -36,25 +43,54 @@ function Cadastro() {
   const togglePasswordVisibility = () => {
     setMostrarSenha((prevMostrar) => !prevMostrar);
   };
+  // verifica mundanças e pega o valor do html - tutu
+  const handleChange = (event) => {
+    setFormDado({
+      ...formDado,
+      [event.target.id]: event.target.value,
+    });
+  };
+//ta validando kkk
+  const validação = () => {
+    let formErrors = {};
+    if (!formDado.nome.trim()) {
+      formErrors.nome = "espaço obrigatorio";
+    }
+    if (!formDado.email.trim()) {
+      formErrors.email = "espaço obrigatorio";
+    }
+    if (!formDado.senha.trim()) {
+      formErrors.senha = "espaço obrigatorio";
+    }
+    setErro(formErrors);
+    return Object.keys(formErrors).length === 0;
+  };
 
-  // Função para lidar com o cadastro
-  const handleCadastro = async () => {
-    try {
-      const response = await axios.post("http://localhost:8000/api/users/create/", {
-        username: nome,
-        email: email,
-        full_name: nome,
-        password: senha,
-      });
-  
-      setMensagem("Usuário cadastrado com sucesso!");
-      console.log("Dados enviados:", response.data);
-    } catch (error) { // tratamento de erro que diz na mensagem de baixo que erro aconteceu 
-      if (error.response) {
-        
-        console.error("Erro no cadastro:", error.response.data);
-      } else {
-        console.error("Erro desconhecido:", error.message);
+  // Função para lidar com o cadastro "alguem me relembra o que e async (seria meio que um aguardar )"-tutu
+  const handleCadastro = async (event) => {
+    event.preventDefault();
+    if (validação()) {
+      try {
+        const response = await axios.post(
+          "http://localhost:8000/api/users/create/",
+          {
+            username: formDado.nome,
+            email: formDado.email,
+            full_name: formDado.nome,
+            password: formDado.senha,
+          }
+        );
+        // pensei que redefinindo o setErro o problema estaria sendo resolvido porem nao foi :c-tutu
+        setErro({});
+        setMensagem("Usuário cadastrado com sucesso!");
+        console.log("Dados enviados:", response.data);
+      } catch (error) {
+        // pq no console ? -tutu
+        if (error.response) {
+          console.error("Erro no cadastro:", error.response.data);
+        } else {
+          console.error("Erro desconhecido:", error.message);
+        }
       }
     }
   };
@@ -63,74 +99,129 @@ function Cadastro() {
     <>
       <div id="home" className={styles.home}>
         <div id="form-header" className={styles.header}>
-
-          <button id="loginBtn" className={`${styles.botaoDiv} ${ pagina === "login" ? styles.botaoDivOn : "" }`} onClick={() => alterarPagina("login")}> Login </button>
-          <button id="cadastroBtn" className={`${styles.botaoDiv} ${ pagina === "cadastro" ? styles.botaoDivOn : "" }`} onClick={() => alterarPagina("cadastro")}> <u>Cadastro</u> </button>
-        
+          <button
+            id="loginBtn"
+            className={`${styles.botaoDiv} ${
+              pagina === "login" ? styles.botaoDivOn : ""
+            }`}
+            onClick={() => alterarPagina("login")}
+          >
+            {" "}
+            Login{" "}
+          </button>
+          <button
+            id="cadastroBtn"
+            className={`${styles.botaoDiv} ${
+              pagina === "cadastro" ? styles.botaoDivOn : ""
+            }`}
+            onClick={() => alterarPagina("cadastro")}
+          >
+            {" "}
+            <u>Cadastro</u>{" "}
+          </button>
         </div>
 
         <div id="form-container" className={styles.box}>
           {pagina === "login" ? (
             <section className={styles.sessaoLogin} id="login">
-
               <input type="email" id="email" placeholder="Email" required />
               <br /> <br />
-
               <div className={styles.passwordContainer}>
-                <input type={mostrarSenha ? "text" : "password"} id="senha" placeholder="Senha" required/>
+                <input
+                  type={mostrarSenha ? "text" : "password"}
+                  id="senha"
+                  placeholder="Senha"
+                  required
+                />
 
-                <span className={styles.togglePassword} onClick={togglePasswordVisibility}>
-                  <img id="eye-icon" src={mostrarSenha ? olhoFechado : olho} alt="Mostrar/Esconder" />
+                <span
+                  className={styles.togglePassword}
+                  onClick={togglePasswordVisibility}
+                >
+                  <img
+                    id="eye-icon"
+                    src={mostrarSenha ? olhoFechado : olho}
+                    alt="Mostrar/Esconder"
+                  />
                 </span>
-
               </div>
-
               <br />
-              <button type="button" className={styles.botao}> Entrar </button>
+              <button type="button" className={styles.botao}>
+                {" "}
+                Entrar{" "}
+              </button>
               <br /> <br />
-
               <div className={styles.esqueceuasenha}>
-
                 <Link to="/esqueceu-senha">
                   <a href="#esqueceu"> Esqueceu a Senha? </a>
                 </Link>
-
               </div>
             </section>
-
           ) : (
-
             <section className={styles.sessaoCadastro} id="cadastro">
-
-              <input type="text" id="nome" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} required />
+              <input
+                type="text"
+                id="nome"
+                placeholder="Nome"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+                required
+              />
+              {erro.nome && <p className={styles.error}>{erro.nome}</p>} 
+              {/* tem 3 dessas um p cada input dps arrumar no css para deixar o contorno vermelho(acho que ficaria melhor) */}
               <br /> <br />
-
-              <input type="email" id="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-
+              <input
+                type="email"
+                id="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              {erro.email && <p className={styles.error}>{erro.email}</p>}
               <br /> <br />
-
               <div className={styles.passwordContainer}>
-
-                <input type={mostrarSenha ? "text" : "password"} id="senha" placeholder="Senha" value={senha} onChange={(e) => setSenha(e.target.value)} required />
-
-                <span className={styles.togglePassword} onClick={togglePasswordVisibility}>
-                  <img id="eye-icon" src={mostrarSenha ? olhoFechado : olho} alt="Mostrar/Esconder"/>
+                <input
+                  type={mostrarSenha ? "text" : "password"}
+                  id="senha"
+                  placeholder="Senha"
+                  value={senha}
+                  onChange={(e) => setSenha(e.target.value)}
+                  required
+                />
+                {erro.senha && <p className={styles.error}>{erro.senha}</p>}
+                <span
+                  className={styles.togglePassword}
+                  onClick={togglePasswordVisibility}
+                >
+                  <img
+                    id="eye-icon"
+                    src={mostrarSenha ? olhoFechado : olho}
+                    alt="Mostrar/Esconder"
+                  />
                 </span>
-
               </div>
-
               <br />
-
-              <button type="button" className={styles.botao} onClick={handleCadastro}> Cadastrar </button>
-
-              {mensagem && <p><br/><br/><br/>{mensagem}</p>}
+              <button
+                type="button"
+                className={styles.botao}
+                onClick={handleCadastro}
+              >
+                {" "}
+                Cadastrar{" "}
+              </button>
+              {mensagem && (
+                <p>
+                  <br />
+                  <br />
+                  <br />
+                  {mensagem}
+                </p>
+              )}
             </section>
           )}
-
         </div>
-
       </div>
-
     </>
   );
 }
